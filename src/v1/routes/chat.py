8daf6d9ac4f975bd321c0b2f61860b52cacdac24
DavidHODs <davidoluwatobi41@gmail.com>
2025-03-31 14:04:15 +0100

    updated readme

--- src/v1/routes/chat.py
+++ src/v1/routes/chat.py
@@ -42,9 +42,4 @@ class ChatRoute:
         description="Broadcast a message to all connected clients in a chat room",
         responses=get_responses(200, 400, 500),
         response_model=BaseResponse[str]
-        
-        
-        
-        
-        
     )
